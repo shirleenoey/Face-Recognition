620641c338e73fa88ff0dd1d705ffef659564473
shirleen <shirleenoey@gmail.com>
2023-03-21 21:08:43 +0700

    connecting front-end to server

--- src/App.js
+++ src/App.js
@@ -21,7 +21,24 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -55,7 +72,22 @@ class App extends Component {
         version: "34ce21a40cc24b6b96ffee54aabff139",
         },
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.setState.user, {entries: count}))
+            })
+        };
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch(err => console.log(err));
   }
 
@@ -78,14 +110,14 @@ class App extends Component {
           ? 
             <div>
               <Logo />
-              <Rank />
-              <ImageLinkForm onInputChange= {this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-              <FaceRecognition box={box} imageUrl={imageUrl}/>
+              <Rank name={this.state.user.name} entries={this.state.user.entries} />
+              <ImageLinkForm onInputChange= {this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
           : (
               route === 'signin' 
-              ? <SignIn onRouteChange={this.onRouteChange} />
-              : <Register onRouteChange={this.onRouteChange} />
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             )
         }
       </div>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
 	return ( 
 		<div>
 			<div className='white f3'>
-				{'Ottie, currently your rank is...'}
+				{`${name} , your current entry count is...:`}
 			</div>
 			<div className='white f1'>
-				{'#5'}
+				{entries}
 			</div>
 		</div>
 	);
