3d2078866ef7b5f8e736e40e3198f0f3b8d720f8
shirleen <shirleenoey@gmail.com>
2023-04-04 16:10:57 +0700

    changing styles

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,14 +5,14 @@ import './ImageLinkForm.css';
 	return ( 
 		<>
 			<div>
-				<p className='f3 washed-blue'>
+				<p className='f4 washed-blue'>
 					{'This Magic Brain will detect faces in your pictures. Give it a try.'}
 				</p>
 			</div>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input onChange= {onInputChange} className='f5 pa2 w-70 center' type='text' placeholder='enter your image URL'/>
-					<button onClick= {onButtonSubmit} className='w-30 grow f5 link ph3 pv dib bg-light-blue pa2'>Detect</button>
+					<input onChange= {onInputChange} className='f6 pa2 w-70 center' type='text' placeholder='enter your image URL'/>
+					<button onClick= {onButtonSubmit} className='w-30 grow f6 link ph3 pv dib bg-light-blue pa2'>Detect</button>
 				</div>
 			</div>
 		</>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,13 +7,13 @@ const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt 
-				style={{width: '7.5rem', 
-								height: '7.5rem', 
+				style={{width: '7rem', 
+								height: '7rem', 
 								tiltMaxAngleX: '90deg', 
 								tiltMaxAngleY: '80deg'}}
 				className='Tilt br2 shadow-2'>
 					<div>
-						<img className='pa2' alt='logo' src={brain} />
+						<img className='pa3' alt='logo' src={brain} />
 					</div>
 			</Tilt>
 		</div>
