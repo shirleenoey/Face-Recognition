60f3481664a7cbeffed7d44291f23fe2b95998bf
shirleen <shirleenoey@gmail.com>
2023-04-04 21:35:38 +0700

    fixing bug

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({name, entries}) => {
 	return ( 
 		<div>
 			<div className='white f4'>
-				{`${name} , your current entry count is...:`}
+				{`${name} , your current entry count is...`}
 			</div>
 			<div className='white f3'>
 				{entries}
